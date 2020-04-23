37b27e924f8f32b8860f944ea1cda919b7c4d681
Lena <lenasurlenet@gmail.com>
2020-04-23 22:04:59 +0200

    Added Trailer to movie template

--- content/movies/parasite.md
+++ content/movies/parasite.md
@@ -4,10 +4,10 @@ public: "Tous"
 director: "Bong Joon-Ho"
 poster: /uploads/parasite.jpg
 date: "2020-06-03"
-trailer: "https://youtu.be/3LYiLq4EHjA"
 duration: "2h12"
 title: "Parasite"
 description: "Toute la famille de Ki-taek est au chômage, et s’intéresse fortement au train de vie de la richissime famille Park. Un jour, leur fils réussit à se faire recommander pour donner des cours particuliers d’anglais chez les Park. C’est le début d’un engrenage incontrôlable, dont personne ne sortira véritablement indemne…"
 actors: " Song Kang-ho, Lee Sun-kyun, Cho Yeo-jeong"
 price: "8.5"
+trailer: "https://youtube.com/embed/RqNcorpHgTk"
 ---
--- src/templates/Movie.vue
+++ src/templates/Movie.vue
@@ -29,67 +29,11 @@
               </div>
               <tabs>
                 <tab name="Synopsis" selected="true">
-                <div class="excerpt">
-                  <h3>Le synopsis</h3>
-                  {{ $page.movie.excerpt }}
-                </div>
-              </tab>
-              <section class="timetable">
-                <tab name="Horaires">
-                <h3>Les séances</h3>
-
-                <subsection class="date">
-                  <h4>Selectionnez une date</h4>
-                <div class="dateCards">
-                  <div class="dateCard">
-                    <p class="day">Mer</p>
-                    <p class="date">21</p>
-                    <p class="month">juin</p>
+                  <div class="excerpt">
+                    <h3>Le synopsis</h3>
+                    {{ $page.movie.excerpt }}
                   </div>
-                  <div class="dateCard__selected">
-                    <p class="day">Jeu</p>
-                    <p class="date">22</p>
-                    <p class="month">juin</p>
-                  </div>
-                  <div class="dateCard">
-                    <p class="day">Ven</p>
-                    <p class="date">23</p>
-                    <p class="month">juin</p>
-                  </div>
-                </div>
-                </subsection>
-                <subsection class="time">
-                  <h4>Sélectionnez une séance</h4>
-                <div class="timeCards">
-                  <div class="timeCard">
-                    <p class="day">12h15</p>
-                  </div>
-                  <div class="timeCard">
-                    <p class="day">17h15</p>
-                  </div>
-                  <div class="timeCard__selected">
-                    <p class="day">20h00</p>
-                  </div>
-                </div>
-                </subsection>
-                <subsection class="ticket">
-                  <h4>Sélectionnez un mombre de places</h4>
-                  <div class="numTicket">
-                    <select name="tickets" id="ticket">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                    </select>
-                    <button class="btn btn__resa">Réserver</button>
-                  </div>
-                </subsection>
-                <div class="cta">
-                  <g-link to="/"><button class="btn btn__primary">Continuer</button></g-link>
-                  <g-link to="/cart"><button class="btn btn__buy">Payer</button></g-link>
-                </div>
-                  
-              </tab>
-              </section>
+                </tab>
               </tabs>
             </div>
           </div>
@@ -137,11 +81,13 @@ query ($path :String!) {
 <script>
 import Tab from "@/components/Tab.vue";
 import Tabs from "@/components/Tabs.vue";
+import Timetable from "@/components/Timetable.vue";
 
 export default {
   components: {
     Tab,
-    Tabs
+    Tabs,
+    Timetable
   }
 };
 </script>
@@ -200,73 +146,6 @@ p {
       background-color: #ff8066;
       padding: 0.75em;
     }
-  }
-}
-
-.dateCards {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-
-  .dateCard {
-    border-radius: 10px;
-    background-color: rgb(250, 164, 250);
-    margin: 1 0.75em;
-    padding: 0 0.75em;
-    align-items: center;
-
-    &__selected {
-      border-radius: 10px;
-      margin: 1 0.75em;
-      padding: 0 0.75em;
-      background-color: rgb(250, 79, 250);
-    }
-  }
-}
-.timeCards {
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-
-  .timeCard {
-    border-radius: 10px;
-    background-color: rgb(250, 164, 250);
-    margin: 1 0.75em;
-    padding: 0 0.25em;
-    align-items: center;
-
-    &__selected {
-      background-color: rgb(250, 79, 250);
-      border-radius: 10px;
-    }
-  }
-}
-
-.numTicket {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.cta {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 1em;
-}
-
-.btn {
-  background-color: orange;
-  border-radius: 10px;
-  margin: 0.25em;
-  padding: 1em 0.75em;
-
-  &__primary {
-    background-color: greenyellow;
-  }
-
-  &__buy {
-    background-color: orangered;
   }
 }
 
