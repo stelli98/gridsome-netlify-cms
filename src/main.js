ba5b90d4244e618be97728acb10cc179a163a26e
Lena <lenasurlenet@gmail.com>
2020-04-25 13:34:43 +0200

    Add scss files. Does not work

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue";
+import '@/assets/sass/main.scss';
+
 
 
 export default function (Vue, {
